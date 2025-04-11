cbb59dc5dba932173a27349c35f9e5d2f6cfabc0
“buiquocdat30” <buiquocdat30@gmail.com>
2025-04-11 19:58:39 +0700

    DAy 2 11042025 20h00 PM

--- back_end/index.js
+++ back_end/index.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/prismaConfig');
-const bodyParser = require('body-parser');
-const uploadRoute = require('./routes/uploadRoute');
-const translateRoute = require('./routes/translateRoute');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/prismaConfig");
+const bodyParser = require("body-parser");
+const uploadRoute = require("./routes/uploadRoute");
+const translateRoute = require("./routes/translateRoute");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+
 // Tăng giới hạn kích thước của payload cho phép
-app.use(bodyParser.json({ limit: '50mb' })); 
+app.use(bodyParser.json({ limit: "100mb" }));
 // Giới hạn 50MB cho JSON body
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 // Route upload
-app.use('/api/upload', uploadRoute);
+app.use("/api/upload", uploadRoute);
 
 //translate
-app.use('/api/translate', translateRoute);
+app.use("/api/translate", translateRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- back_end/routes/uploadRoute.js
+++ back_end/routes/uploadRoute.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const { handleUpload } = require('../controllers/uploadController');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { handleUpload } = require("../controllers/uploadController");
 
 const router = express.Router();
 
-const upload = multer({ dest: 'uploads/' });
-
-router.post('/', upload.single('file'), handleUpload);
+router.post("/", handleUpload);
 
 module.exports = router;
