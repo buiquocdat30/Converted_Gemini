1f2b0e87b41b6ea5593808571bb30be710bf738f
buiquocdat30 <buiquocdat30@gmail.com>
2025-04-25 09:54:58 +0700

    DAY 10 25042025 10h00 AM

--- front_end/src/components/TranslationInfoPanel.jsx
+++ front_end/src/components/TranslationInfoPanel.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import '../css/TranslationInfoPanel.css';
-
-
+import React from "react";
+import "../css/TranslationInfoPanel.css";
 
 const TranslationInfoPanel = ({
   totalChapters,
+  totalWords,
   averageWordsPerChapter,
 }) => {
-  
-
   return (
     <div className="tip-container">
       <h3 className="tip-title">📊 Thông tin tệp đã tải lên</h3>
-      <p><strong>Tổng số chương:</strong> {totalChapters}</p>
-      <p><strong>Số chữ trung bình mỗi chương:</strong> {averageWordsPerChapter}</p>
-
-      
+      <div className="tip-body">
+        <p>
+          <strong>📘 Tổng số chương:</strong> {totalChapters}
+        </p>
+        <p>
+          <strong>📝 Tổng số từ:</strong> {totalWords}
+        </p>
+        <p>
+          <strong>📊 Số chữ trung bình mỗi chương:</strong>{" "}
+          {averageWordsPerChapter}
+        </p>
+      </div>
     </div>
   );
 };
