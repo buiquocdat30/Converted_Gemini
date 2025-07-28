41755ed6089ae36428044534c0472abcd068cc02
buiquocdat30 <buiquocdat30@gmail.com>
2025-07-28 16:15:27 +0700

    DAY 72 28072025 16H30PM ĐANG CB CẤU HÌNH WORKER RIÊNG SO VỚI BE SERVER

--- back_end/utils/queue.js
+++ back_end/utils/queue.js
@@ -4,6 +4,7 @@ const IORedis = require('ioredis');
 const connection = new IORedis({
   host: 'localhost',
   port: 6379,
+  maxRetriesPerRequest: null, // BẮT BUỘC cho BullMQ!
 });
 
 console.log('[QUEUE] Initializing BullMQ queue with rate limiter 15 jobs/minute');
